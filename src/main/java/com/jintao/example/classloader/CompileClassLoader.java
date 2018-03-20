4d0ed15df0898d01ce1d8b05ea2a828635b6d835
yangjinjue <yangjinjue@zhiweidata.com>
2018-03-20 16:37:21 +0800

    自定义编译类加载器

---
+++ src/main/java/com/jintao/example/classloader/CompileClassLoader.java
@@ -0,0 +1,10 @@
+package com.jintao.example.classloader;
+
+/**
+ * Created with IDEA
+ * author:jinjueYang
+ * Date:2018/3/20
+ * Time:15:54
+ */
+public class CompileClassLoader extends ClassLoader {
+}
