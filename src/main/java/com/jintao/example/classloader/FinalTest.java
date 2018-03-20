c7b25dd5200bacae11994690657e192fb3e69f6c
yangjinjue <yangjinjue@zhiweidata.com>
2018-03-20 14:51:15 +0800

    增加动态常量测试

--- src/main/java/com/jintao/example/classloader/FinalTest.java
+++ src/main/java/com/jintao/example/classloader/FinalTest.java
@@ -12,10 +12,12 @@ class MyTest{
         System.out.println("静态初始化代码块.....");
     }
     static final String compileConstant = "常量";
+    static final String dynamicConstant = System.currentTimeMillis() + "常量";
 }
 public class FinalTest {
     public static void main(String[] args) {
         System.out.println(MyTest.compileConstant);
+//        System.out.println(MyTest.dynamicConstant);
     }
 }
 
@@ -23,4 +25,5 @@ public class FinalTest {
  * 当某个类变量(静态常量)被final修饰，而且它的值可以在编译阶段就确定下来，那么程序在其它地方使用时，
  * 实际上并没有使用该类变量，而是相当于使用常量。
  * MyTest.compileConstant该行代码在编译时都被替换成常量
+ * MyTest.dynamicConstant是一个动态的值，在编译阶段不可确定，调用该变量会导致类初始化
  **/
